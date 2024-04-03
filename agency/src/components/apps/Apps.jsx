b7ffd1a83a0be8c1731fbeb6fa7f3bb040ece0b4
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2024-04-02 22:52:56 -0500

    5

--- agency/src/components/apps/Apps.jsx
+++ agency/src/components/apps/Apps.jsx
@@ -15,12 +15,39 @@ export default function Apps() {
     <p>Descubre formas flexibles de ganar dinero con estas aplicaciones, permitiéndote equilibrar tus responsabilidades personales y profesionales</p>
     </div>
     <div>
-      <div>
+      
+
+      <div className="appsDiv">
+        <div>
+          <h2>GlOW</h2>
+          <p></p>
+        </div>
       <img src={glow}></img>
+      </div>
+      <div className="appsDiv">
+      <div>
+          <h2>LIVCHAT</h2>
+          <p></p>
+        </div>
       <img src={livchat}></img>
       </div>
+      <div className="appsDiv">
       <div>
+          <h2>TIGO</h2>
+          <h6>FUNCIÓN DE LA APP</h6>
+          <ul>
+          <li>Aplicación de videollamadas 1x1: machts y privados.</li>
+          <li>Obtienes ganancias por:  matchs monetizadas, minuto de privados, regalos y mensajes.</li>
+          <li></li>
+          </ul>
+        </div>
       <img src={tigo}></img>
+      </div>
+      <div className="appsDiv">
+      <div>
+          <h2>MAKO</h2>
+          <p></p>
+        </div>
       <img src={mako}></img>
       </div>
     </div>
