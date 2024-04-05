deb43d1be1f23d14bc7c301a3eb2b234c4b56455
claurojass23 <128934493+claurojass23@users.noreply.github.com>
2024-04-04 23:26:26 -0500

    chat

--- agency/src/components/inicio/Inicio.jsx
+++ agency/src/components/inicio/Inicio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import imagen from '../../img/ig.png'
+import imagen from '../../img/gush.png'
 import './inicio.css'
+import { Link } from 'react-router-dom'
+
 export default function Inicio() {
   return (
     <>
     <div className='inicio1'>
-    <img src={imagen} className='imagenInicio'></img>
+    <Link to='/Inicio'><img src={imagen} className='imagenInicio'></img></Link>
     </div>
      
     </>
